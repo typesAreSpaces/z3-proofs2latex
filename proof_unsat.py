770a8594c5905f7347f0057697f85c3e784d7c82
jose <jabelcastellanosjoo@unm.edu>
2020-06-17 03:58:53 -0600

    Minor changes.

--- proof_unsat.py
+++ proof_unsat.py
@@ -9,12 +9,10 @@ set_option(max_args=10000000,
            proof=True)
 set_pp_option("max_indent", 0)
 set_pp_option("max_width", 100000)
-# set_pp_option("spacer.arith_solver", 0)
 
 # s = Solver();
-# s = SolverFor("QF_IDL")
 s = SolverFor("QF_LIA")
-s.set(fp.spacer.arith.solver, 0)
+s.set("smt.arith.solver", 4)
 
 # s.from_file("./tests/a.smt2")
 # s.from_file("./tests/paper_example.smt2")
